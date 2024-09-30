9419554ad5e69910eaed7f55391bb28cdb780352
Peter Curtin <98424367+pmcurtin@users.noreply.github.com>
2024-09-30 15:04:11 -0400

    Update __init__.py

--- src/torchrunx/__init__.py
+++ src/torchrunx/__init__.py
@@ -1,9 +1,10 @@
-from .launcher import Launcher, launch
+from .launcher import Launcher, launch, LaunchResult
 from .logging_utils import add_filter_to_handler, file_handler, stream_handler
 
 __all__ = [
     "Launcher",
     "launch",
+    "LaunchResult",
     "add_filter_to_handler",
     "file_handler",
     "stream_handler",
