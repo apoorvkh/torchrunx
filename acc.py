be485e900503509f69cdbe0a3260f05e7c1dab69
Peter Curtin <peter_curtin@brown.edu>
2025-01-29 11:56:24 -0500

    accelerator example

--- acc.py
+++ acc.py
@@ -1,12 +1,3 @@
-# Examples
-
-## Training GPT-2 on WikiText
-
-We will show examples of how to use `torchrunx` alongside several deep learning libraries to train a GPT-2 (small) model with text data from WikiText.
-
-### Accelerate
-
-```python
 from pathlib import Path
 
 import torch
@@ -15,6 +6,8 @@ from datasets import load_dataset
 from torch import nn
 from torch.utils.data import Dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
+
+import torchrunx
 
 
 class GPT2CausalLMDataset(Dataset):
@@ -72,96 +65,15 @@ def train():
         optimizer.step()
 
     return model
-```
 
-```python
-import torchrunx
 
 if __name__ == "__main__":
     Path("output").mkdir(exist_ok=True)
     results = torchrunx.launch(
         func=train,
         hostnames=["localhost"],
-        workers_per_host=1,
+        workers_per_host=2,
     )
 
     trained_model: nn.Module = results.rank(0)
     torch.save(trained_model.state_dict(), "output/model.pth")
-```
-
-### HF Trainer
-
-```python
-import torch
-from datasets import load_dataset
-from torch import nn
-from torch.utils.data import Dataset
-from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments
-
-
-class GPT2CausalLMDataset(Dataset):
-    def __init__(self, text_dataset):
-        self.dataset = text_dataset
-        self.tokenizer = AutoTokenizer.from_pretrained("gpt2")
-        self.tokenizer.pad_token = self.tokenizer.eos_token
-        self.max_length = 1024
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, idx):
-        encoded = self.tokenizer(
-            self.dataset[idx]["text"],
-            max_length=self.max_length,
-            truncation=True,
-            padding="max_length",
-            return_tensors="pt",
-        )
-
-        input_ids = encoded.input_ids.squeeze()
-        labels = input_ids.clone()
-
-        return {"input_ids": input_ids, "labels": labels}
-
-
-def train():
-    model = AutoModelForCausalLM.from_pretrained("gpt2")
-    wikitext_train = load_dataset(
-        "Salesforce/wikitext", name="wikitext-2-v1", split="train"
-    )
-    train_dataset = GPT2CausalLMDataset(text_dataset=wikitext_train)
-
-    trainer = Trainer(
-        model=model,
-        args=TrainingArguments(
-            output_dir="output",
-            per_device_train_batch_size=16,
-            max_steps=10,
-        ),
-        train_dataset=train_dataset,
-    )
-
-    trainer.train()
-
-    return model
-```
-
-```python
-import torchrunx
-
-if __name__ == "__main__":
-    results = torchrunx.launch(
-        func=train,
-        hostnames=["localhost"],
-        workers_per_host=1,
-    )
-
-    trained_model: nn.Module = results.rank(0)
-    torch.save(trained_model.state_dict(), "output/model.pth")
-```
-
-### DeepSpeed
-
-### PyTorch Lightning
-
-### MosaicML Composer
