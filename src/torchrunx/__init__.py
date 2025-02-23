5a8845281b938dc6546e6c3d273ee3b524ad41f3
apoorvkh <mail@apoorvkh.com>
2025-02-23 15:43:51 -0500

    torchrunx.__version__

--- src/torchrunx/__init__.py
+++ src/torchrunx/__init__.py
@@ -1,5 +1,9 @@
+import importlib.metadata
+
 from .launcher import DEFAULT_ENV_VARS_FOR_COPY, Launcher, LaunchResult
 from .utils.errors import AgentFailedError, WorkerFailedError
+
+__version__ = importlib.metadata.version(__package__ or __name__)
 
 __all__ = [  # noqa: RUF022
     "DEFAULT_ENV_VARS_FOR_COPY",
